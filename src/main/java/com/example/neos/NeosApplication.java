834fbed0b53a82de65300a09c9b623db51703e6d
kristian <kancleris.kristian@gmail.com>
2022-11-26 15:31:37 +0000

    initial setup

---
+++ src/main/java/com/example/neos/NeosApplication.java
@@ -0,0 +1,13 @@
+package com.example.neos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeosApplication.class, args);
+	}
+
+}
