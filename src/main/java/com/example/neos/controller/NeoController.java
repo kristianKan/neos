e411ecb9ab1a561db9a1775b0de1a643aa3935b4
kristian <kancleris.kristian@gmail.com>
2022-11-28 19:25:19 +0000

    plug everything together

--- src/main/java/com/example/neos/controller/NeoController.java
+++ src/main/java/com/example/neos/controller/NeoController.java
@@ -14,12 +14,14 @@ public class NeoController {
     @Autowired
     NeoService neoService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(method = RequestMethod.GET, path = "feed/{start}&{end}")
     public Feed getNeosFeed(@PathVariable("start") String start, @PathVariable("end") String end){
         return neoService.getNeoFeed(start, end);
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "id/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping(method = RequestMethod.GET, path = "neo/{id}")
     public Neo getNeoById(@PathVariable("id") String id){
         return neoService.getNeoById(id);
     }
